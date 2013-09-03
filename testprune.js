91e802e80e9445ebca675cf8a8bd35a6fee80cda
Alex Nichol <alexnichol@comcast.net>
2013-09-02 23:49:39 -0400

    first commit

--- testprune.js
+++
@@ -1,8 +0,0 @@
-var cube = require('cube');
-var prune = require('prune');
-
-var allChoices = cube.edges.echoose.generate();
-console.log('made all choices with count of ' + allChoices.length);
-var symTable = new prune.SymTable(allChoices);
-console.log('made symmetries');
-console.log('there are ' + symTable.perfectHashCount() + ' things.');
