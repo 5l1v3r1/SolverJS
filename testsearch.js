b1df45dcf6620994fc9e5eb5720aa08dd0e9d78c
Alex Nichol <alexnichol@comcast.net>
2013-09-05 22:10:56 -0400

    fixed stupid application issue but didn't fix big issue

--- testsearch.js
+++ testsearch.js
@@ -5,14 +5,14 @@ var cube = require('cube');
 console.log('creating context');
 var standardContext = context.standardContext();
 
-console.log('creating search')
+console.log('creating search');
 var eSlice = new cube.edges.echoose.EChoose();
 var eo = new cube.edges.eo.EdgeOrientations(null, eSlice);
 var co = new cube.corners.comap.CoMap();
 var search = new Search(standardContext, eo, co);
 
 console.log('running search');
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 4; i++) {
     console.log('Exploring depth ' + i + ' with ' + search.nodeCount + ' cosets found...');
     search.nextIteration(i);
 }
